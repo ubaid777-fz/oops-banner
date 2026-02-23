931c91b59109a7881c040152e7b70c2300091c1b
Ubaid <ubaidmohammad2007@gmail.com>
2026-02-23 20:26:19 +0530

    UC4: Refactored banner using String array and loop for better modularity

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,34 +2,41 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("   ",
-                " ***** ",
-                " ***** ",
-                " ***** ",
-                " ***** "));
+        String[] banner = {
 
-        System.out.println(String.join("   ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*      "));
+                String.join("   ",
+                        " ***** ",
+                        " ***** ",
+                        " ***** ",
+                        " ***** "),
 
-        System.out.println(String.join("   ",
-                "*     *",
-                " ***** ",
-                " ***** ",
-                " ***** "));
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
 
-        System.out.println(String.join("   ",
-                "*     *",
-                "*      ",
-                "*      ",
-                "      *"));
+                String.join("   ",
+                        "*     *",
+                        " ***** ",
+                        " ***** ",
+                        " ***** "),
 
-        System.out.println(String.join("   ",
-                " ***** ",
-                "*      ",
-                " ***** ",
-                " ***** "));
+                String.join("   ",
+                        "*     *",
+                        "*      ",
+                        "*      ",
+                        "      *"),
+
+                String.join("   ",
+                        " ***** ",
+                        "*      ",
+                        " ***** ",
+                        " ***** ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
